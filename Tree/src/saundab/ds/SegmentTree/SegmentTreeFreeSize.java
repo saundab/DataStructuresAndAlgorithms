5411ab60705df62ce751baf3c6c6166ad4f29e54
saundab <abhi.saund@gmail.com>
2018-10-10 13:51:54 +0530

    implemented the update function in segment tree

--- Tree/src/saundab/ds/SegmentTree/SegmentTreeFreeSize.java
+++ Tree/src/saundab/ds/SegmentTree/SegmentTreeFreeSize.java
@@ -20,12 +20,24 @@ public class SegmentTreeFreeSize {
 	public static void main(String[] args) {
 		int[] arr= {11,12,13,14,15,16};
 		int[] st=new SegmentTreeFreeSize ().createSegmentTree(arr);
-		new SegmentTreeFreeSize ().updateSegmentTree(arr, st, 1, 110);
+		new SegmentTreeFreeSize ().updateSegmentTree(arr, st, 0, 0, arr.length-1, 1, 10);
+		System.out.println("done");
 	}
 
 	//UPDATE
-	private void updateSegmentTree(int[] arr, int[] st, int idx, int val) {
-		
+	private void updateSegmentTree(int[] arr, int[] st, int stIdx, int start, int end, int idx, int val) {
+		if(start==end) {
+			arr[idx]=val;
+			st[stIdx]=val;
+		}else {
+			int mid=start + (end-start)/2;
+			if(idx>=start && idx<=mid) {
+				updateSegmentTree(arr, st, 2*stIdx+1, start, mid, idx, val);
+			}else {
+				updateSegmentTree(arr, st, 2*stIdx+2, mid+1, end, idx, val);
+			}
+			st[stIdx]=st[2*stIdx+1]+st[2*stIdx+2];
+		}
 		
 	}
 
